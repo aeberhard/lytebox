395954cdc70e3ab1a89a4c20ac01cb85feaaacc2
TobiasKrais <tk@design-to-use.de>
2014-07-23 23:24:23 +0200

    Create modul-video-input.php

---
+++ modules/modul-video-input.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * --------------------------------------------------------------------
+ *
+ * Modul zum hinzufuegen von Videos mit Lytebox
+ *
+ * --------------------------------------------------------------------
+ */
+?>
+
+<strong style="display:block;width:185px;">Video ausw&auml;hlen ...</strong><br />
+REX_MEDIA_BUTTON[1]
+Breite des Videos (Standard 800px): <input type="text" style="width:70px;" name="VALUE[1]" value="REX_VALUE[1]" /><br />
+H&ouml;he des Videos (Standard 600px): <input type="text" style="width:70px;" name="VALUE[2]" value="REX_VALUE[2]" /><br />
+<br />
+<strong style="display:block;width:185px;">Vorschaubild ausw&auml;hlen ...</strong><br />
+REX_MEDIA_BUTTON[2]
+Breite Vorschaubild (Teaserspalte maximal 155, sonst 650):
+<input type="text" style="width:70px;" name="VALUE[3]" value="REX_VALUE[3]" /><br />
