d7b2078f1ae71c12d1cad5af68a2107cbacce394
TobiasKrais <tk@design-to-use.de>
2014-07-23 23:25:11 +0200

    Create modul-youtube-input.php

---
+++ modules/modul-youtube-input.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * --------------------------------------------------------------------
+ *
+ * Modul zum hinzufuegen von YoutubeVideos mit Lytebox
+ *
+ * --------------------------------------------------------------------
+ */
+?>
+
+<strong>Youtube Video URL:</strong> <input type="text" style="width:300px;" name="VALUE[1]" value="REX_VALUE[1]" /><br />
+Breite des Videos (Standard 800px): <input type="text" style="width:70px;" name="VALUE[2]" value="REX_VALUE[2]" /><br />
+H&ouml;he des Videos (Standard 600px): <input type="text" style="width:70px;" name="VALUE[3]" value="REX_VALUE[3]" /><br />
+<br />
+<strong style="display:block;width:185px;">Vorschaubild ausw&auml;hlen ...</strong><br />
+REX_MEDIA_BUTTON[1]
+Breite Vorschaubild in Pixel (Standard 200):
+<input type="text" style="width:70px;" name="VALUE[4]" value="REX_VALUE[4]" /><br />
